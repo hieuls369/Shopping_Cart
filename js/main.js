6f495ccf6c273c33f6585b1bd1420b40769736cc
hieuls369 <hieuls369@gmail.com>
2020-05-14 19:33:48 +0700

    update the increasing and decreasing product in cart

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 //target all the element has the same class
 let carts = document.querySelectorAll('.add-cart');
 
-
+var check = true;
+console.log(carts);
 //create and store product information
 let product = [
     {
@@ -36,34 +37,38 @@ onLoadCartNumbers();
 //add a cart event for each product
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers(product[i]);
+        cartNumbers(product[i], true);
         onLoadCartNumbers();
-        totalCost(product[i]);
+        totalCost(product[i], true);
     });
 
 }
 
 
 //count number of the product has been clicked
-function cartNumbers(product) {
+function cartNumbers(product, check) {
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
     //if the first time click on cart set the productNumber to 1 else plus 1
     if (productNumbers) {
+        if(check){
         localStorage.setItem('cartNumbers', productNumbers + 1);
         //document.querySelectorAll('.cart span').textContent = productNumbers + 1;
+        }else{
+        localStorage.setItem('cartNumbers', productNumbers - 1);
+        }
     } else {
         localStorage.setItem('cartNumbers', 1);
         //document.querySelectorAll('.cart span').textContent = 1;
     }
 
-    setItem(product);
+    setItem(product, check);
 
 
 }
 //store the product has been click to the localStorage
-function setItem(product) {
+function setItem(product, check) {
     //get all the product int the cart
     let cartItems = localStorage.getItem('productsInCart');
 
@@ -79,7 +84,11 @@ function setItem(product) {
                 [product.tag]: product
             }
         }
+        if(check){
         cartItems[product.tag].inCart += 1;
+        }else{
+            cartItems[product.tag].inCart -= 1;
+        }
     } else {
         product.inCart = 1;
         cartItems = {
@@ -100,30 +109,46 @@ function onLoadCartNumbers() {
 }
 
 
-
-function totalCost(product) {
+//store the total of all the product has been purchase
+function totalCost(product, check) {
+    //get the totalCost in localStorage
     var total = localStorage.getItem('totalCost');
-
+    //parse from string into integer
+    total = JSON.parse(total);
+    //if is the first time purchase the total set to product price
+    //else get total price of all product
     if (total == null) {
-        total = JSON.parse(total);
         total = product.price;
     } else {
+        if(check){
         total += product.price;
+        }else{
+            total -= product.price;
+        }
     }
 
     localStorage.setItem('totalCost', total);
 }
 
+
+//Display all the product has been purchase to the cart.htm
 function displayCart() {
+    //get productsInCart and totalCost in the localStorage
     let cartItems = localStorage.getItem("productsInCart");
     let cartCost = localStorage.getItem("totalCost");
     cartItems = JSON.parse(cartItems);
-    
+
+    //get the element has a class of .product in home.htm
     let productContainer = document.querySelector('.products');
 
+    //if the cartItems and productContainer is existed 
+    //then display the product to the cart
     if (cartItems && productContainer) {
+        //set the innerHTML of the .products to be empty
         productContainer.innerHTML = '';
+        //check all of the items in the cartItems
         Object.values(cartItems).map(item => {
+            //concat the information of each product to the cart.htm
             productContainer.innerHTML += `
             <div class="product">
                 <ion-icon name="close-circle"></ion-icon>
@@ -132,14 +157,14 @@ function displayCart() {
             </div>
             <div class="price">$${item.price},00</div>
             <div class="quantity">
-                <ion-icon name="arrow-back-circle-outline"></ion-icon>
+                <ion-icon class="decrease ${item.tag}" name="arrow-back-circle-outline"></ion-icon>
                 <span>${item.inCart}</span>
-                <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+                <ion-icon class="increase ${item.tag}" name="arrow-forward-circle-outline"></ion-icon>
             </div>
             <div class="total">$${item.inCart * item.price},00</div>
             `;
         });
-
+        //this is for the total price you need to pay for all the products
         productContainer.innerHTML += `
             <div class="basketTotalContainer">
                 <h4 class="basketTotalTitle">
@@ -150,6 +175,48 @@ function displayCart() {
                 </h4>
             </div>
         `
+        addCart(cartItems);
     }
 }
 displayCart();
+//delete the item in the cart
+function deleteCart() {
+
+}
+//increase or decrease the item in the cart
+function addCart(product1){
+   
+    console.log(product1);
+
+    let increaseCart = document.querySelectorAll('.increase');
+    let decreaseCart = document.querySelectorAll('.decrease');
+    for (let i = 0; i < increaseCart.length; i++) {
+        increaseCart[i].addEventListener('click', () => {
+            for (let j = 0; j < product.length; j++) {
+                if(increaseCart[i].classList[1] == product[j].tag){
+                    cartNumbers(product1[product[j].tag], true);
+                    onLoadCartNumbers();
+                    totalCost(product1[product[j].tag], true);
+                    displayCart();
+                }
+                
+            }
+            
+        });
+    }
+    for (let i = 0; i < decreaseCart.length; i++) {
+        decreaseCart[i].addEventListener('click', () => {
+            for (let j = 0; j < product.length; j++) {
+                if(increaseCart[i].classList[1] == product[j].tag){
+                    cartNumbers(product1[product[j].tag], false);
+                    onLoadCartNumbers();
+                    totalCost(product1[product[j].tag], false);
+                    displayCart();
+                }
+                
+            }
+            
+        });
+    }
+}
+
